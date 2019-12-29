6a595b2e3ebde43d8d76119f7f012e4031ead679
Boonpakorn.non <38358165+boonpakornn@users.noreply.github.com>
2019-12-29 15:57:53 +0700

    Add Landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { ReportComponent } from './report.component';
+import { ReportListComponent } from './report/report-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    ReportComponent
+    ReportComponent,
+    ReportListComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [ReportComponent]
